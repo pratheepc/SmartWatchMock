28fe365fa79622c08f95a04f12a35f917e17ff69
pratheepc <pratheepc09@gmail.com>
2024-02-07 23:28:35 +0530

    Added stopwatch functionality

--- script.js
+++ script.js
@@ -75,6 +75,89 @@ function goBack() {
   document.getElementById("DLVRY").style.display = "none";
 }
 
-function stopwatch(){
-    
+let hour = 00;
+let minute = 00;
+let second = 00;
+
+function startTimer(event) {
+  console.log("startTimer");
+  timer = true;
+  document.getElementById("play").style.display = "none";
+  document.getElementById("pause").style.display = "flex";
+  console.log("yes");
+  stopWatch();
+}
+
+function pauseTimer() {
+  timer = false;
+  document.getElementById("play").style.display = "flex";
+  document.getElementById("pause").style.display = "none";
+}
+
+function resetTimer() {
+  timer = false;
+  hour = 0;
+  minute = 0;
+  second = 0;
+  count = 0;
+  document.getElementById("hour").innerHTML = "00";
+  document.getElementById("min").innerHTML = "00";
+  document.getElementById("sec").innerHTML = "00";
+}
+
+function stopTimer() {
+  timer = false;
+  if (hour < 10) {
+    hrString = "0" + hour;
+  }
+
+  if (minute < 10) {
+    minString = "0" + minute;
+  }
+
+  if (second < 10) {
+    secString = "0" + second;
+  }
+  document.getElementById("hour2").innerHTML = hrString;
+  document.getElementById("min2").innerHTML = minString;
+  document.getElementById("sec2").innerHTML = secString;
+}
+
+function stopWatch() {
+  if (timer) {
+    second++;
+
+    if (second == 60) {
+      minute++;
+      second = 0;
+    }
+
+    if (minute == 60) {
+      hour++;
+      minute = 0;
+      second = 0;
+    }
+
+    let hrString = hour;
+    let minString = minute;
+    let secString = second;
+
+    if (hour < 10) {
+      hrString = "0" + hrString;
+    }
+
+    if (minute < 10) {
+      minString = "0" + minString;
+    }
+
+    if (second < 10) {
+      secString = "0" + secString;
+    }
+
+    document.getElementById("hour").innerHTML = hrString;
+    document.getElementById("min").innerHTML = minString;
+    document.getElementById("sec").innerHTML = secString;
+
+    setTimeout(stopWatch, 1000);
+  }
 }
